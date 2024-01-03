336460529ba08411c184156f2248331931452146
masayaO <okuda4645@gmail.com>
2024-01-03 11:29:17 +0900

    onChange

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
@@ -6,7 +6,7 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-import { EditorThemeClasses } from "lexical";
+import { EditorState, EditorThemeClasses } from "lexical";
 
 const exampleTheme: EditorThemeClasses = {
   ltr: "ltr",
@@ -36,12 +36,39 @@ function onError(error: Error) {
   console.error(error);
 }
 
+interface OnChangePluginProps {
+  onChange: (editorState: EditorState) => void;
+}
+
+// OnChangePluginコンポーネント
+const OnChangePlugin: React.FC<OnChangePluginProps> = ({ onChange }) => {
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      onChange(editorState);
+    });
+  }, [editor, onChange]);
+
+  return null; // このコンポーネントは何も表示しない
+};
+
 export function Editor() {
+  const [editorStateJSON, setEditorStateJSON] = useState<EditorState>();
+
+  function onChange(editorState: EditorState) {
+    // toJSONを呼び出し、シリアライズ可能な文字列を生成
+    // const serializedEditorState = editorState.toJSON();
+    // JSON文字列に変換
+    setEditorStateJSON(editorState);
+  }
   const initialConfig = {
     namespace: "MyEditor",
     theme: exampleTheme,
     onError,
   };
+  useEffect(() => {
+    console.log(editorStateJSON);
+  }, [editorStateJSON]);
 
   return (
     <div className="editor-container">
@@ -53,6 +80,7 @@ export function Editor() {
         />
         <HistoryPlugin />
         <MyCustomAutoFocusPlugin />
+        <OnChangePlugin onChange={onChange} />
       </LexicalComposer>
     </div>
   );
